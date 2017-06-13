39eebed6cc3ddfef9fd188e6ebbb90ba670ef6f9
Johan Brattemark <johan.brattemark@gmail.com>
2017-06-13 16:21:22 -0700

    Add route handlers for Query, Read, Create

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,45 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/books', (req, res, next) => {
+  knex('books')
+    .orderBy('id')
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/books/:id', (req, res, next) => {
+  let id = req.params.id * 1;
+  knex('books')
+    .where('id', id)
+    .then((book) => {
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/books', (req, res, next) => {
+  knex('books')
+    // .insert({ name: req.body.name })
+    .insert(req.body)
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
 
 module.exports = router;
