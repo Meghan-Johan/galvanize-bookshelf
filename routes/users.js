6be086d78db517f2637db13d90408b750ce21bca
Meghan Prestemon <m.m.hares@gmail.com>
2017-06-14 15:14:20 -0700

    Implement all for Part 3. No Bonus.

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const express = require('express');
-
+const knex = require('../knex');
+const bcrypt = require('bcrypt-as-promised');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  const password = req.body.password;
+  const saltRounds = 10;
+  bcrypt.hash(password, saltRounds)
+    .then(passwordHash => {
+      knex('users')
+      .insert({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        hashed_password: passwordHash
+      }, '*')
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((err) => {
+        next(err);
+      });
+    })
+})
 
 module.exports = router;
