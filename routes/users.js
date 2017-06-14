a5a5f04ae70c6be70d090ed3533529249e2cefa1
Meghan Prestemon <m.m.hares@gmail.com>
2017-06-14 16:23:47 -0700

    Fix bugs to pass test for user registration

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const knex = require('../knex');
 const bcrypt = require('bcrypt-as-promised');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
@@ -13,13 +14,13 @@ router.post('/users', (req, res, next) => {
     .then(passwordHash => {
       knex('users')
       .insert({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
         email: req.body.email,
         hashed_password: passwordHash
-      }, '*')
+      }, ['id', 'first_name', 'last_name', 'email'])
       .then((user) => {
-        res.send(user);
+        res.send(camelizeKeys(user[0]));
       })
       .catch((err) => {
         next(err);
