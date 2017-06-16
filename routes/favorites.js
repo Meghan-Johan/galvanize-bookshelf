353d0ddf7f572f9de1db680a4e03b4b14bd030f5
Johan Brattemark <johan.brattemark@gmail.com>
2017-06-16 14:54:20 -0700

    Add GET routes for /favorites

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,10 +1,32 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+const { camelizeKeys, decamelizeKeys } = require('humps');
+
+router.get('/favorites', (req, res, next) => {
+  knex('favorites')
+  .innerJoin('books', 'books.id', 'favorites.book_id')
+  .then((favorites) => {
+    res.send(camelizeKeys(favorites));
+  })
+  .catch((err) => {
+    next(err);
+  });
+})
+
+router.get('/favorites/check', (req, res, next) => {
+  knex('favorites')
+  .count('*')
+  .where('book_id', req.query.bookId)
+  .then(countObj => {
+    let count = countObj[0].count * 1;
+    res.send(count > 0);
+  })
+})
 
 module.exports = router;
